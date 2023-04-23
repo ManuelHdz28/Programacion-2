b241e9a4d40a280a1a7094be66b26788c8c5ba6b
Manuel Hernandez <manuel.hernandez28@outlook.com>
2023-04-23 15:25:09 +0200

    iniciando repocitorio

---
+++ ExamenAuxiliarCatedra2021/src/examenauxiliarcatedra2021/ExamenAuxiliarCatedra2021.java
@@ -0,0 +1,13 @@
+package examenauxiliarcatedra2021;
+
+
+public class ExamenAuxiliarCatedra2021 {
+
+    public static void main(String[] args) {
+       
+        frmCandidatos frm = new frmCandidatos();
+        frm.show();
+        
+    }
+    
+}
