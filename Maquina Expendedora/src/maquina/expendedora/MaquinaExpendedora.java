92bc39f2ddfd1171a602254f6023f90cda7a547a
Manuel Hernandez <manuel.hernandez28@outlook.com>
2023-05-02 09:13:37 +0200

    nuevo ejercicio maquina expendedora

---
+++ Maquina Expendedora/src/maquina/expendedora/MaquinaExpendedora.java
@@ -0,0 +1,12 @@
+package maquina.expendedora;
+
+public class MaquinaExpendedora {
+
+    public static void main(String[] args) {
+        
+        maquina frm = new maquina();
+        frm.show();
+        
+    }
+    
+}
